97d42b3bf58459a37a6da2a12cb7fcb8737acda2
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-04-15 19:35:32 +0200

    create register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,49 @@
+import { redirect } from 'next/navigation';
+
+import { createClient } from '@/utils/supabase/server';
+import { register } from './actions';
+
+export default async function Register() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (data?.user) {
+    redirect('/private');
+  }
+
+  return (
+    <main className='w-full min-h-screen flex flex-col items-center p-24'>
+      <h1 className='text-2xl font-bold'>Sign Up</h1>
+      <form
+        action={register}
+        className='flex flex-col gap-6 justify-center items-start p-6'>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='email'>Email</label>
+          <input
+            className='p-1 rounded text-black'
+            id='email'
+            name='email'
+            type='email'
+            required
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='password'>Password</label>
+          <input
+            className='p-1 rounded text-black'
+            id='password'
+            name='password'
+            type='password'
+            required
+          />
+        </div>
+        <button
+          type='submit'
+          className='self-stretch rounded px-6 py-2 mt-1 bg-white text-black font-semibold'>
+          Sign Up
+        </button>
+      </form>
+    </main>
+  );
+}
