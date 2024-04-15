a78c4d5e9c67de95781dc351249079f41a6da9d5
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-04-15 19:58:39 +0200

    create example UI for easy navigation

---
+++ app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <main className='w-full min-h-screen flex justify-center p-24'>
+      <h1 className='text-3xl font-bold'>Something went wrong! 😕</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen justify-center p-24'>
+    <main className='flex flex-col min-h-screen items-center gap-5 p-24'>
       <h1 className='text-3xl font-bold'>Welcome to Chit-Chat</h1>
     </main>
   );
