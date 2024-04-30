5771d4e7bd3c9d5c6cea52261abc9b62501df2fe
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-04-30 22:22:38 +0200

    add styles

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -8,18 +8,20 @@ interface ChatProps {
 
 const Chat = ({ chatName, lastMessage, lastMessageDate }: ChatProps) => {
   return (
-    <div className='flex flex-row items-center gap-2 rounded-lg bg-black/40 px-4 py-3 text-slate-100 shadow-lg'>
+    <div className='flex cursor-pointer flex-row items-center gap-2 rounded-lg bg-black/40 px-3 py-3 text-slate-100 shadow-md transition-transform hover:scale-[101%]'>
       <Avatar>
         <AvatarImage src='https://github.com/shadcn.png' />
         <AvatarFallback>{chatName}&apos;s avatar</AvatarFallback>
       </Avatar>
-      <section className='flex-grow'>
-        <div className='flex flex-row justify-between'>
-          <div className='text-lg font-bold'>{chatName}</div>
-          <div className='text-sm text-muted-foreground'>{lastMessageDate}</div>
+      <div className='flex-grow'>
+        <div className='flex flex-row justify-between gap-2'>
+          <div className='text-lg font-bold leading-tight'>{chatName}</div>
+          <div className='flex-shrink-0 text-sm text-muted-foreground'>
+            {lastMessageDate}
+          </div>
         </div>
-        <div>{lastMessage}</div>
-      </section>
+        <div className='pt-1'>{lastMessage}</div>
+      </div>
     </div>
   );
 };
